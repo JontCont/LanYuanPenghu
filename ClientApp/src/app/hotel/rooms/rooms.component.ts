8f2a2eba34cf0552768a042159a4bd94f70105ec
jontcont <52921053+JontCont@users.noreply.github.com>
2023-01-15 09:33:43 +0800

    css 調整

--- ClientApp/src/app/hotel/rooms/rooms.component.ts
+++ ClientApp/src/app/hotel/rooms/rooms.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomsComponent implements OnInit {
   title :string ="最新消息";
+  lineUrl : string = "https://liff.line.me/1645278921-kWRPP32q/?accountId=346nabfy";
 
   constructor() { }
 
--- ClientApp/src/app/hotel/shared/header-v1/header-v1.component.ts
+++ ClientApp/src/app/hotel/shared/header-v1/header-v1.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-v1.component.scss']
 })
 export class HeaderV1Component implements OnInit {
-
+  active : string ="close";
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onMenuClick():void {
+    console.log(1);
+    if(this.active == "close"){
+      this.active = "open";
+    }else{
+      this.active ="close";
+    }
+  }
 }
