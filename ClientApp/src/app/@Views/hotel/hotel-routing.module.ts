b1e9178f08773a766aea68954e4b1b8ead9b4a43
jontcont <52921053+JontCont@users.noreply.github.com>
2023-10-10 19:59:40 +0800

    暫時加入

---
+++ ClientApp/src/app/@Views/hotel/hotel-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PhotoComponent } from './photo/photo.component';
+import { NewsComponent } from './news/news.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { SpecialtyComponent } from './specialty/specialty.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: 'home', component:HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'photo', component: PhotoComponent},
+      { path: 'news', component: NewsComponent },
+      { path: 'rooms', component: RoomsComponent },
+      { path: 'specialty', component:SpecialtyComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' }
+    ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HotelRoutingModule { }
